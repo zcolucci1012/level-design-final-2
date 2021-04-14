acbd34042cff58685d24157250e691c63b254f63
zcolucci1012 <32983507+zcolucci1012@users.noreply.github.com>
2021-04-14 14:38:47 -0400

    UI and Logic
    - Added logic to killing boss and opening portal
    - Added boss health
    - Moved player health
    - Fixed Boss AI

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Boss : Enemy
 {
+    public Slider healthBar;
+
     protected override void Move()
     {
 
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        healthBar.value = this.currentHealth;
+    }
+
+    protected override void EnemyDies()
+    {
+        GameObject.Find("Canvas").GetComponent<UIController>().ShowBossHealth(false);
+        GameObject.Find("Canvas").GetComponent<UIController>().UpdateObjective("OBJECTIVE: Enter the portal.");
+        GameObject.Find("Hole").GetComponent<EnterPortal>().SetOpen(true);
+        base.EnemyDies();
     }
 }
